74139a8512e08094b0497608c5e0a38fc9402662
ChanSeul <whckstmf@gmail.com>
2018-12-20 02:32:40 +0900

    grep까지 보내기 완료

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #define oops(m,x) {perror(m); exit(x);}
 
 void be_cat(int togrep[2],int tosort[2],int fromsort[2]);
@@ -24,7 +26,7 @@ void main()
 	{
 		be_grep(togrep,tosort,fromsort);
 	}
-	if(pid[1]==0)//sort process
+	else if(pid[1]==0)//sort process
 	{
 		be_sort(togrep,tosort,fromsort);
 	}
@@ -35,8 +37,9 @@ void main()
 }
 void be_cat(int togrep[2],int tosort[2],int fromsort[2])
 {
+	char grep_argument[BUFSIZ]="\"",buff[BUFSIZ];
 	FILE *fpout,*fpin;
-
+	
 	close(tosort[0]);
 	close(tosort[1]);
 	close(fromsort[1]);
@@ -44,9 +47,32 @@ void be_cat(int togrep[2],int tosort[2],int fromsort[2])
 
 	fpout = fdopen(togrep[1],"w");
 	fpin = fdopen(fromsort[0],"r");
+
+	printf("Input Ingredients you have\n");
+   while(1)
+   {   
+		scanf("%s",buff);
+      if(buff[0]=='q' || strcmp(buff,"quit")==0)
+		{
+			grep_argument[strlen(grep_argument)-1]='\0';
+			grep_argument[strlen(grep_argument)-1]='\0';
+			grep_argument[strlen(grep_argument)]='\"';
+			break;
+		}
+		strcat(grep_argument,"[");
+      strcat(grep_argument,buff);
+		strcat(grep_argument,"]");
+		strcat(grep_argument,"/|");
+		
+		
+    }
+	fprintf(fpout,"%s",grep_argument);
+	fflush(fpout);
+
 }
 void be_grep(int togrep[2],int tosort[2],int fromsort[2])
 {
+	char argument_buff[BUFSIZ],*argument;
 	FILE *fpout,*fpin;
 
 	close(fromsort[0]);
@@ -56,6 +82,12 @@ void be_grep(int togrep[2],int tosort[2],int fromsort[2])
 
 	fpout = fdopen(tosort[1],"w");
 	fpin = fdopen(togrep[0],"r");
+
+	fgets(argument_buff,BUFSIZ,fpin);
+	argument = (char*)malloc(strlen(argument_buff)+1);
+	strcpy(argument,argument_buff);
+
+	//execlp("grep","grep","-c",argument,"*",NULL);
 }
 void be_sort(int togrep[2],int tosort[2],int fromsort[2])
 {
